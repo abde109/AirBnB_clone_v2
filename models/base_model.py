61145d0845c678a2de8c04f14bafa87d91b0747c
abde109 <abderrahimkhadri2@gmail.com>
2023-11-22 16:47:53 +0100

    fixing console.py errors output

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self, **kwargs):
+    def __init__(self, *args,**kwargs):
         """Instantiates a new model with given attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
