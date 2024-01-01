4e70ace52fdd77b1078889e1844744d48ce24823
abde109 <abderrahimkhadri2@gmail.com>
2024-01-01 20:24:38 +0000

    Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ method returns cities by states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ method to remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
